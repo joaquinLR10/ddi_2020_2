f5359c07c154d706a276b9fcc42c812090890d02
joaquinLR10 <joaquin.larranaga@uabc.edu.mx>
2020-10-08 23:48:21 -0700

    programa, practica 2 lista

---
+++ practica1/Assets/Scripts/codigo_p2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class codigo_p2 : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+         string [] inscritos = {"alberto","carlos","daniel","joaquin","kennya","omar"};
+         string nombre ="joaquin";
+         comparacion(nombre,inscritos);
+    }
+
+        void comparacion(string nombre,string [] inscritos){
+        int i,flag=0;
+        for(i=0;i<inscritos.Length;i++)
+            if(nombre.CompareTo(inscritos[i])==0){
+               flag=1;
+                Debug.Log("El alumno inscrito es :"+inscritos[i]);
+                
+            }
+           if(flag!=1)
+          Debug.Log("el alumno no esta inscrito");
+          
+         
+    }
+}
+
+
+
+
+
