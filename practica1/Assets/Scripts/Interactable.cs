8d3cf7627631d41e812aa6345d881aa83ff12267
joaquinLR10 <joaquin.larranaga@uabc.edu.mx>
2020-10-15 19:39:46 -0700

    practica 3

---
+++ practica1/Assets/Scripts/Interactable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+   public AudioClip sonido = null;
+   public float volumen = 1.0f;//indica el volumen del sonido 
+   protected Transform posicion = null;
+    void Start()
+    {
+        posicion= transform;
+    }
+
+   
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if(!other.CompareTag("Player"))
+        {
+            return;
+        }
+       if(sonido)
+       AudioSource.PlayClipAtPoint(sonido,posicion.position,volumen);
+
+       
+        
+    }
+}
