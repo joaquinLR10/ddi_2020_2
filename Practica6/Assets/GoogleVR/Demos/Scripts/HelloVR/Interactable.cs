098d59fdd318b53f681fefbf27935cf7ded4bf75
joaquinLR10 <joaquin.larranaga@uabc.edu.mx>
2020-11-18 14:07:20 -0800

    Practica6

---
+++ Practica6/Assets/GoogleVR/Demos/Scripts/HelloVR/Interactable.cs
@@ -0,0 +1,47 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Interactable : MonoBehaviour
+{
+    bool isInsideZone = false;
+   
+
+    public virtual void Interact()
+    {
+        
+        Debug.Log("Ejecutando interacción...");
+      
+        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+       // Input.GetKeyDown(KeyCode.I)
+        if(isInsideZone)
+        {
+            Interact();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+        isInsideZone = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+        isInsideZone = false;
+    }
+}
---
+++ Practica6/Assets/GoogleVR/Demos/Scripts/HelloVR/Sonido.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sonido : MonoBehaviour
+{
+   public AudioClip sonido = null;
+   public float volumen = 1.0f;//indica el volumen del sonido 
+   protected Transform posicion = null;
+   
+    void Start()
+    {
+        posicion= transform;
+    }
+
+   
+   
+    void update(){
+        if(CompareTag("Ham"))
+        {
+       if(sonido)
+       AudioSource.PlayClipAtPoint(sonido,posicion.position,volumen);
+       Debug.Log("cogiendo jamon....");
+        }
+        else
+        {
+       if(sonido)
+       AudioSource.PlayClipAtPoint(sonido,posicion.position,volumen);
+       Debug.Log("cogiendo posion......");
+         
+        }
+       
+    }
+      
+       
+        
+    }
