ef576045d284f6e7ae5c3b4bed938d1502c3fc36
joaquinLR10 <joaquin.larranaga@uabc.edu.mx>
2020-11-23 14:16:32 -0800

    PRACTICA7

---
+++ Practica7/Assets/Sonidoo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Sonidoo : MonoBehaviour
+{
+   public AudioClip sonido = null;
+   public float volumen = 1.0f;//indica el volumen del sonido 
+   protected Transform posicion = null;
+   
+   
+    void Start()
+    {
+        posicion= transform;
+    }
+
+   
+
+    void update(){
+        if(CompareTag("Respawn"))
+     if(sonido)
+       {
+           AudioSource.PlayClipAtPoint(sonido,posicion.position,volumen);
+       }
+       
+    
+      
+       
+        
+    }
+}
